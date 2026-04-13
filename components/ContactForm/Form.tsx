037c9c569248596e8b6a4671f8578b84268bcaa9
abdulwasea89 <165668178+abdulwasea89@users.noreply.github.com>
2026-04-13 23:47:10 +0500

    update

--- components/ContactForm/Form.tsx
+++ components/ContactForm/Form.tsx
@@ -1,15 +1,15 @@
 "use client"
 import { motion } from "framer-motion"
-import Loader from "../ui/Loader";
-import SuccessForm from "../ui/SuccessForm";
-import { FormEvent, useState } from "react";
-import { formVariants } from "@/animation/varients";
+import Loader from "../ui/Loader"
+import SuccessForm from "../ui/SuccessForm"
+import { FormEvent, useState } from "react"
+import { formVariants } from "@/animation/varients"
+
 export default function Form() {
 
     const [isSubmitting, setIsSubmitting] = useState<boolean>(false)
     const [loader, setLoader] = useState<boolean>(false)
     const [errorOnSubmit, setErrorOnSubmit] = useState(false)
-
 
     const [data, setData] = useState({
         name: "",
@@ -18,10 +18,10 @@ export default function Form() {
     })
 
     const handleSubmit = (e: FormEvent) => {
-        e.preventDefault();
+        e.preventDefault()
 
-        setIsSubmitting(true);
-        setLoader(true);
+        setIsSubmitting(true)
+        setLoader(true)
 
         // TODO: API CALL
 
@@ -30,20 +30,15 @@ export default function Form() {
                 name: "",
                 email: "",
                 message: "",
-            });
-            setLoader(false);
-            setErrorOnSubmit(false);
+            })
+            setLoader(false)
+            setErrorOnSubmit(false)
 
             setTimeout(() => {
-                setIsSubmitting(false);
-
-            }, 7000);
-        }, 4000);
-
-
-
-    };
-
+                setIsSubmitting(false)
+            }, 7000)
+        }, 4000)
+    }
 
     return (
         <motion.div
@@ -51,41 +46,41 @@ export default function Form() {
             animate="visible"
             variants={formVariants}
             viewport={{ once: true }}
-            className={`${isSubmitting ? "gap-0" : 'gap-0'} flex flex-col w-full`}
+            className="flex flex-col w-full"
         >
-            {isSubmitting ?
+            {isSubmitting ? (
                 <div className="h-full min-h-[350px] w-full relative flex flex-col items-center justify-center bg-dark-gray-2 rounded-xl border border-dark-gray-3 select-none">
-                    {loader ? <Loader /> : <> <SuccessForm action={errorOnSubmit ? "error" : "success"} /> </>}
+                    {loader ? (
+                        <Loader />
+                    ) : (
+                        <SuccessForm action={errorOnSubmit ? "error" : "success"} />
+                    )}
                 </div>
-                :
+            ) : (
                 <form method="POST" onSubmit={handleSubmit} className="grid gap-4">
+
                     {/* Input Fields */}
                     <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-                        <div className="">
-                            <input
-                                className={`appearance-none w-full leading-[1.4em] outline-none border-none p-4 rounded-lg font-medium text-base placeholder:text-light-gray-1 text-very-light-gray bg-dark-gray-2`}
-                                name="Name"
-                                placeholder="Name"
-                                type="text"
-                                aria-label="Full Name"
-                                value={data.name}
-                                onChange={(e) => setData({ ...data, name: e.target.value })}
-
-                            />
-                        </div>
-                        <div className="">
-                            <input
-                                className="appearance-none w-full leading-[1.4em] outline-none border-none p-4 rounded-lg font-medium text-base placeholder:text-light-gray-1 text-very-light-gray bg-dark-gray-2"
-                                name="Email"
-                                placeholder="Email"
-                                type="email"
-                                aria-label="Email Address"
-                                value={data.email}
-                                onChange={(e) => setData({ ...data, email: e.target.value })}
-
-                            />
-                        </div>
+                        <input
+                            className="appearance-none w-full leading-[1.4em] outline-none border-none p-4 rounded-lg font-medium text-base placeholder:text-light-gray-1 text-very-light-gray bg-dark-gray-2"
+                            name="Name"
+                            placeholder="Name"
+                            type="text"
+                            aria-label="Full Name"
+                            value={data.name}
+                            onChange={(e) => setData({ ...data, name: e.target.value })}
+                        />
+                        <input
+                            className="appearance-none w-full leading-[1.4em] outline-none border-none p-4 rounded-lg font-medium text-base placeholder:text-light-gray-1 text-very-light-gray bg-dark-gray-2"
+                            name="Email"
+                            placeholder="Email"
+                            type="email"
+                            aria-label="Email Address"
+                            value={data.email}
+                            onChange={(e) => setData({ ...data, email: e.target.value })}
+                        />
                     </div>
+
                     {/* Textarea */}
                     <textarea
                         className="appearance-none w-full leading-[1.4em] outline-none border-none p-4 rounded-lg font-medium text-base placeholder:text-light-gray-1 text-very-light-gray bg-dark-gray-2 resize-y min-h-56 max-h-96"
@@ -95,7 +90,6 @@ export default function Form() {
                         aria-label="Message"
                         value={data.message}
                         onChange={(e) => setData({ ...data, message: e.target.value })}
-
                     />
 
                     {/* Submit Button */}
@@ -105,8 +99,9 @@ export default function Form() {
                     >
                         Send Your Message
                     </button>
+
                 </form>
-            }
+            )}
 
             <div />
         </motion.div>
