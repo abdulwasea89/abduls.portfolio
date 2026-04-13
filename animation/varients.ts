dc24196d43528581d1d1eac26b3c6e8f491646ab
abdulwasea89 <165668178+abdulwasea89@users.noreply.github.com>
2026-04-13 19:01:02 +0000

    update the packages and files

--- animation/varients.ts
+++ animation/varients.ts
@@ -16,3 +16,27 @@ export const formVariants: Variants = {
         transition,
     },
 }
+
+export const listVariants: Variants = {
+    hidden: {
+        opacity: 0,
+    },
+    visible: {
+        opacity: 1,
+        transition: {
+            staggerChildren: 0.1,
+        },
+    },
+}
+
+export const itemVariants: Variants = {
+    hidden: {
+        opacity: 0,
+        y: 20,
+    },
+    visible: {
+        opacity: 1,
+        y: 0,
+        transition,
+    },
+}
